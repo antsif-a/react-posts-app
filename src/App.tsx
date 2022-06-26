4de9eb8561257721ae13c196496ca9f4260309d1
antsif-a <summet.dev@gmail.com>
2022-06-26 03:05:51 +0300

    refactor: move App.tsx alongside index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './ui/navbar/Navbar';
-import Posts from '../routes/Posts';
-import Post from '../routes/Post';
-import About from '../routes/About';
-import '../styles/main.scss';
+import Navbar from './components/ui/navbar/Navbar';
+import Posts from './routes/Posts';
+import Post from './routes/Post';
+import About from './routes/About';
+import './styles/main.scss';
 
 function App() {
     return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
